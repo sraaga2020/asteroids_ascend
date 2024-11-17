b80e64cdea861ac1dd02782c6fc07b795868ed58
sraaga2020 <63835764+sraaga2020@users.noreply.github.com>
2024-11-16 23:06:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-# In app.py
 import streamlit as st
 from dashboard import run_dashboard
-from asteroids import run_asteroid_tracker
-
+from asteroids import run_asteroids
+# Manage navigation state
 if 'current_app' not in st.session_state:
     st.session_state.current_app = "home"
 
 if st.session_state.current_app == "home":
     st.title("Home Page")
-    if st.button("Open Dashboard"):
+    st.write("Welcome! Choose an app to run:")
+
+    if st.button("Asteroid Dashboard"):
         st.session_state.current_app = "dashboard"
-    if st.button("Open Asteroid Tracker"):
-        st.session_state.current_app = "asteroid"
+
+    if st.button("Alien Asteroid Mission"):
+        st.session_state.current_app = "asteroids"
 
 elif st.session_state.current_app == "dashboard":
     run_dashboard()
 
-elif st.session_state.current_app == "asteroid":
-    run_asteroid_tracker()
+elif st.session_state.current_app == "asteroids":
+    run_asteroids()
+
+    if st.button("Back to Home"):
+        st.session_state.current_app = "home"
